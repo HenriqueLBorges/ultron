a016b94d11ce4471eb3b752c7f8424a23125444b
Henrique Borges <henriquelb96@gmail.com>
2019-04-26 01:56:09 -0300

    search query added

---
+++ app.js
@@ -0,0 +1,7 @@
+let twitter = require("./services/twitter")
+
+console.log(process.env.consumer_key)
+    console.log(process.env.consumer_secret)
+        console.log(process.env.access_token_key)
+            console.log(process.env.access_token_secret)
+twitter.search()
--- services/twitter.js
+++ services/twitter.js
@@ -1,11 +1,35 @@
 let config = require("../config/config");
 let Twitter = require("twitter");
- 
+
 let client = new Twitter({
-  consumer_key: config.consumerAPIKey,
-  consumer_secret: config.consumerAPIKeySecret,
-  access_token_key: config.accessToken,
-  access_token_secret: config.accessTokenSecret
+    consumer_key: config.consumerAPIKey,
+    consumer_secret: config.consumerAPIKeySecret,
+    access_token_key: config.accessToken,
+    access_token_secret: config.accessTokenSecret
 });
- 
+
 //Define methods here
+let search = () => {
+    let params = {
+        "q": "%22quero%20assistir%20vingadores%22OR%22quero%20ver%20vingadores%22OR%22quero%20testemunhar%20vingadores%22OR%22preciso%20assistir%20vingadores%22OR%22preciso%20ver%20vingadores%22OR%22preciso%20testemunhar%20vingadores%22OR%22tenho%20que%20assistir%20vingadores%22OR%22tenho%20que%20ver%20vingadores%22OR%22tenho%20que%20testemunhar%20vingadores%22",
+        "result_type": "mixed",
+        "count": "10",
+        "lang": "pt",
+        "since_id": ""
+    }
+    result = []
+    client.get("https://api.twitter.com/1.1/search/tweets.json", params, (error, tweets, response) => {
+        if (!error) {
+            tweets.statuses.map((tweet) => {
+                result.push({"user": tweet.user.screen_name, "id": tweet.id_str})
+            });
+        } else {
+            console.error(error);
+        }
+        console.log(result);
+    });
+}
+
+module.exports = {
+    search: search
+}
