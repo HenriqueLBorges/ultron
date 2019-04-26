f540d7c73d2d9817390559a439035706f82e14b0
Henrique Borges <henriquelb96@gmail.com>
2019-04-26 00:24:43 -0300

    update

---
+++ config/config.js
@@ -0,0 +1,11 @@
+let consumerAPIKey = ""
+let consumerAPIKeySecret = ""
+let accessToken = ""
+let accessTokenSecret = ""
+
+module.exports = {
+    consumerAPIKey: consumerAPIKey,
+    consumerAPIKeySecret: consumerAPIKeySecret,
+    accessToken: accessToken,
+    accessTokenSecret: accessTokenSecret
+}
